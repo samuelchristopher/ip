cf858f60d91933d6256b8acfcb2721be39e0d5e9
Sam Chris <thesamchris@gmail.com>
2020-08-24 20:44:16 +0800

    add greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    public static void separator() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        separator();
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        separator();
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        separator();
     }
 }
