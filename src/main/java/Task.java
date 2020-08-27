5752518c6dc97ebc4b8ed2798aaa3ce443a14c0b
Sam Chris <thesamchris@gmail.com>
2020-08-27 18:55:02 +0800

    add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    public static int numberOfTasks = 0;
+    public static Task[] all = new Task[100];
+    public boolean completed = false;
+    public String title = "";
+
+    public Task(String title) {
+        this.title = title;
+        numberOfTasks++;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean getStatus() {
+        return completed;
+    }
+}
