7e966265680a4d8f6d101cc0db8da099b48291ce
Sam Chris <thesamchris@gmail.com>
2020-09-09 12:26:39 +0800

    added exception files

---
+++ src/main/java/EmptyEventException.java
@@ -0,0 +1,6 @@
+public class EmptyEventException extends DukeException {
+    public static String errorMessage() {
+        return "☹ OOPS!!! Event description or timing cannot be empty. \n" +
+                "Please use event <event name> /at <event timing>.";
+    }
+}
